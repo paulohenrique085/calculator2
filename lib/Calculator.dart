d3a4b0e92d13fd6f2ded53704ed9924add8f3570
paulohenrique085 <paulohnq85@outlook.com>
2021-03-14 12:30:50 -0300

    tela

---
+++ lib/Calculator.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculator"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.black,
+            height: 100,
+            width: 600,
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "1",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "2",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "3",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "4",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "5",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "6",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "7",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "8",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                ),
+                child: Container(
+                  color: Colors.orange,
+                  width: 65,
+                  height: 65,
+                  child: GestureDetector(
+                    child: Center(
+                      child: Text(
+                        "9",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
